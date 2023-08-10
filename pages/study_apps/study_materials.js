6e683f380cf7e97b3d7d77a3db0deffda4509848
Mo7ammedXD <mohammedkab2001@gmail.com>
2023-08-11 00:06:26 +0200

    1-edit CSS on subjects page _2-add _margin to the study material _3-edit JavaScript on subjects page to make a choice if user want go to subjects or question page

---
+++ pages/study_apps/study_materials.js
@@ -0,0 +1,99 @@
+
+  const materialsData = [
+    {
+      Name: "منهج محمد د. الفرجاني (رحمه الله)",
+      image: "/imgs/presentations.png",
+      link : "www.google.com"
+    },
+    {
+      Name: "IEEE 829",
+      image: "/imgs/note.png",
+      link : "https://www.youtube.com/watch?v=8dWL3wF_OMw&t=20392s"
+    },
+    {
+      Name: "IEEE 829 (1) ",
+      image: "/imgs/note.png",
+      link : "https://www.youtube.com/watch?v=8dWL3wF_OMw&t=20392s"
+    },
+ 
+    {
+      Name: "IEEE 829 (2) ",
+      image: "/imgs/note.png",
+      link : "https://www.youtube.com/watch?v=8dWL3wF_OMw&t=20392s"
+    },
+    {
+      Name: "اختبار برمجيات ",
+      image: "/imgs/folder.png",
+      link : "www.google.com"
+    },
+    {
+      Name: "IEEE 829(3)",
+      image: "/imgs/note.png",
+      link : "https://www.youtube.com/watch?v=8dWL3wF_OMw&t=20392s"
+    },
+    {
+      Name: "IEEE 829(4)",
+      image: "/imgs/note.png",
+      link : "https://www.youtube.com/watch?v=8dWL3wF_OMw&t=20392s"
+    },
+    {
+      Name: "IEEE 829(5)",
+      image: "/imgs/note.png",
+      link : "https://www.youtube.com/watch?v=8dWL3wF_OMw&t=20392s"
+    },
+  ];
+
+  function createMaterialsList() {
+    const materialsList = document.getElementById("materials-list");
+
+    materialsData.forEach(material => {
+      const li = document.createElement("li");
+      li.className = "mnahg col-12 mb-4 me-2 ms-2 mt-2 pr-2 d-flex justify-content-end align-items-end";
+
+      const a = document.createElement("a");
+      a.className = "pdfitems";
+      a.href = material.link;
+
+      const text = document.createTextNode(material.Name);
+      a.appendChild(text);
+
+      const img = document.createElement("img");
+      img.className = "imgass";
+      img.src = material.image;
+      img.alt = material.Name;
+
+      li.appendChild(a);
+      li.appendChild(img);
+
+      materialsList.appendChild(li);
+    });
+  }
+
+  createMaterialsList();
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var linkElements = document.querySelectorAll('a');
+    linkElements.forEach(function(linkElement) {
+      if (linkElement.textContent.trim() === 'الفصل السابع') {
+        linkElement.addEventListener("click", function(event) {
+          event.preventDefault();
+          var targetUrl = "../subjects_page/subjects.html";
+          window.location.href = targetUrl;
+        });
+      }
+      if (linkElement.textContent.trim() === 'اسئلة') {
+        linkElement.addEventListener("click", function(event) {
+          event.preventDefault();
+          var targetUrl = "../questions_page/questions.html";
+          window.location.href = targetUrl;
+        });
+      }
+      if (linkElement.textContent.trim() === 'اختر تخصصك') {
+        linkElement.addEventListener("click", function(event) {
+          event.preventDefault();
+          var targetUrl = "../semester/semester.html";
+          window.location.href = targetUrl;
+        });
+      }
+    });
+  });
--- pages/subjects_page/subjects.js
+++ pages/subjects_page/subjects.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
   var linkElements = document.querySelectorAll('a');
+  var buttonElements = document.querySelectorAll('button');
+
   linkElements.forEach(function(linkElement) {
-    if (linkElement.textContent.trim() === 'اختبار برمجات') {
+    if (linkElement.textContent.trim() === 'الفصل السابع') {
       linkElement.addEventListener("click", function(event) {
         event.preventDefault();
-        var targetUrl = "../study_materials_page/study_materials.html";
+        var targetUrl = "../subjects_page/subjects.html";
         window.location.href = targetUrl;
       });
     }
@@ -12,6 +14,24 @@ document.addEventListener("DOMContentLoaded", function() {
       linkElement.addEventListener("click", function(event) {
         event.preventDefault();
         var targetUrl = "../semester/semester.html";
+        window.location.href = targetUrl;
+      });
+    }
+  });
+
+  
+  buttonElements.forEach(function(buttonElement) {
+    if (buttonElement.textContent.trim() === 'الاسئلة') {
+      buttonElement.addEventListener("click", function(event) {
+        event.preventDefault();
+        var targetUrl = "../questions_page/questions.html";
+        window.location.href = targetUrl;
+      });
+    }
+    if (buttonElement.textContent.trim() === 'المناهج') {
+      buttonElement.addEventListener("click", function(event) {
+        event.preventDefault();
+        var targetUrl = "../study_materials_page/study_materials.html";
         window.location.href = targetUrl;
       });
     }
